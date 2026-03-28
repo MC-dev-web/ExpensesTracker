f63fd083f381cc42c6084403d161fc9580ab2a4e
Mc-Cesar <83863543+MC-dev-web@users.noreply.github.com>
2026-03-28 13:28:45 +0800

    Add files via upload
    TimesheetController

---
+++ Controller/TransactionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IActionResult GetList()
+        {
+            return Ok("Test owkay");
+        }
+    }
+}
